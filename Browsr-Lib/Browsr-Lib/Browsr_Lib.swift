44bfbdcb234f9d370a1a2cf169379d43439ee1cd
Afonso Rodrigues <afonso.rodrigues@zurich.ch>
2023-02-12 19:45:44 +0000

    Removed unnused code

--- Browsr-Lib/Browsr-Lib/Browsr_Lib.swift
+++ Browsr-Lib/Browsr-Lib/Browsr_Lib.swift
@@ -8,6 +8,14 @@
 import Foundation
 import Combine
 
+public enum BrowsrLibError: Error {
+    case generic
+    case invalidUrl
+    case decodingError
+    case invalidStatusCode
+    case custom(error: Error)
+}
+
 final public class Browsr_Lib {
 
     private let urlSession: URLSession
@@ -19,68 +27,54 @@ final public class Browsr_Lib {
     }
     
     func getOrganizations(request: OrganizationsRequest) -> AnyPublisher<OrganizationsPagedResponse, Error> {
-        return Future {[weak self] promise in
-            guard let self else {
-                promise(Result.failure(APIError.generic))
-                return
+        guard let url = request.endpoint.components.url else {
+            return Fail(outputType: OrganizationsPagedResponse.self,
+                        failure: BrowsrLibError.invalidUrl).eraseToAnyPublisher()
+        }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.allHTTPHeaderFields = request.headers
+        urlRequest.httpMethod = request.endpoint.method.rawValue
+        
+        var nextPageUrl = ""
+        
+        return self.urlSession.dataTaskPublisher(for: urlRequest).tryMap { element in
+            guard let httpResponse = element.response as? HTTPURLResponse,
+                  (200..<300).contains(httpResponse.statusCode) else {
+                throw BrowsrLibError.invalidStatusCode
             }
             
-            guard let url = request.endpoint.components.url else {
-                promise(Result.failure(APIError.invalidUrl))
-                return
+            if let headers = httpResponse.allHeaderFields as? [String: String] {
+                nextPageUrl = headers["Link"] ?? ""
             }
             
-            var urlRequest = URLRequest(url: url)
-            urlRequest.allHTTPHeaderFields = request.headers
-            urlRequest.httpMethod = request.endpoint.method.rawValue
-            
-            var nextPageUrl = ""
-            
-            let dataTaskPublisher = self.urlSession.dataTaskPublisher(for: urlRequest).tryMap { element in
-                guard let httpResponse = element.response as? HTTPURLResponse,
-                      (200..<300).contains(httpResponse.statusCode) else {
-                    throw APIError.invalidStatusCode
-                }
-                
-                if let headers = httpResponse.allHeaderFields as? [String: String] {
-                    nextPageUrl = headers["Link"] ?? ""
-                }
-                
-                return element.data
-            }.decode(type: [Organization].self, decoder: self.decoder).map { organizations in
-                return OrganizationsPagedResponse(results: organizations, nextPageUrl: nextPageUrl)
-            }
-            
+            return element.data
+        }.decode(type: [Organization].self, decoder: self.decoder).map { organizations in
+            return OrganizationsPagedResponse(results: organizations, nextPageUrl: nextPageUrl)
         }.eraseToAnyPublisher()
     }
     
     func getOrganization(name: String) -> AnyPublisher<Organization, Error> {
-        return Future {[weak self] promise in
-            guard let self else {
-                promise(Result.failure(APIError.generic))
-                return
+        let request = OrganizationsRequest(endpoint: .search(name: name))
+        
+        guard let url = request.endpoint.components.url else {
+            return Fail(outputType: Organization.self,
+                        failure: BrowsrLibError.invalidUrl).eraseToAnyPublisher()
+        }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.allHTTPHeaderFields = request.headers
+        urlRequest.httpMethod = request.endpoint.method.rawValue
+        
+        return self.urlSession.dataTaskPublisher(for: urlRequest).tryMap { element in
+            guard let httpResponse = element.response as? HTTPURLResponse,
+                  (200..<300).contains(httpResponse.statusCode) else {
+                throw BrowsrLibError.invalidStatusCode
             }
-            
-            let request = OrganizationsRequest(endpoint: .search(name: name))
-            
-            guard let url = request.endpoint.components.url else {
-                promise(Result.failure(APIError.invalidUrl))
-                return
-            }
-            
-            var urlRequest = URLRequest(url: url)
-            urlRequest.allHTTPHeaderFields = request.headers
-            urlRequest.httpMethod = request.endpoint.method.rawValue
-            
-            let dataTaskPublisher = self.urlSession.dataTaskPublisher(for: urlRequest).tryMap { element in
-                guard let httpResponse = element.response as? HTTPURLResponse,
-                      (200..<300).contains(httpResponse.statusCode) else {
-                    throw APIError.invalidStatusCode
-                }
-                return element.data
-            }.decode(type: Organization.self, decoder: self.decoder)
-            
-        }.eraseToAnyPublisher()
+            return element.data
+        }.decode(type: Organization.self, decoder: self.decoder)
+            .eraseToAnyPublisher()
+
     }
     
 }
